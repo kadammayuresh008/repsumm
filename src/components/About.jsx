a37c026c5c2d1a9c351a5f7234031f3d1a89204d
unknown <kadammayuresh199@gmail.com>
2021-10-28 01:03:25 +0530

    about page done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,80 @@
+import Heading from '../HelperComponents/Heading';
+import ReactRoundedImage from 'react-rounded-image';
+import DefaultImage from '../Images/DefaultImage.jpg';
 
-const About=()=>{
+const About = () => {
+
+    const AboutImageStyle = {
+        height: "300px",
+        backgroundColor: "aqua",
+        position: "relative",
+        textAlign: "center",
+    }
+
+    const AboutImageText = {
+        position: "absolute",
+        left: "50px",
+        bottom: "70px",
+        color: "white",
+        opacity: 1.0,
+        textShadow: "3px 3px black",
+    }
+
     return (
-        <div className="container">
+        <center>
+            <div className="AboutImageStyle" style={AboutImageStyle}>
+                <h1 className="AboutImageText" style={AboutImageText}>About Research Paper Summarizer </h1>
+            </div>
             <br></br>
+            <div className="container">
+                <Heading name="Why RPS?"></Heading>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac luctus mi. Aenean nec augue dolor.
+                    Sed varius sodales augue eu pulvinar. Etiam et risus varius, sollicitudin enim mollis, ornare nunc.
+                    Donec in convallis dolor, vitae placerat dolor. Vivamus eget elit lobortis, venenatis ex a, maximus
+                    elit. Cras dictum fringilla mi a dapibus. Vivamus lobortis, augue et luctus molestie, metus nunc
+                    semper lorem, at maximus velit felis quis neque.
+                </p>
+            </div>
             <br></br>
-            <h3>
-            This is the about page
-            </h3>
-        </div>
+            <div>
+                <Heading name="Team Members"></Heading>
+                <div className="row container">
+                    <div className="col-sm-3">
+                        <ReactRoundedImage
+                            image={DefaultImage}
+                            roundedColor="aqua"
+                            roundedSize="5"
+                        ></ReactRoundedImage>
+                        <h5>Mayuresh Kadam</h5>
+                    </div>
+                    <div className="col-sm-3">
+                        <ReactRoundedImage
+                            image={DefaultImage}
+                            roundedColor="aqua"
+                            roundedSize="5"
+                        ></ReactRoundedImage>
+                        <h5>Mahek Baru</h5>
+                    </div>
+                    <div className="col-sm-3">
+                        <ReactRoundedImage
+                            image={DefaultImage}
+                            roundedColor="aqua"
+                            roundedSize="5"
+                        ></ReactRoundedImage>
+                        <h5>Jainam Mehta</h5>
+                    </div>
+                    <div className="col-sm-3">
+                        <ReactRoundedImage
+                            image={DefaultImage}
+                            roundedColor="aqua"
+                            roundedSize="5"
+                        ></ReactRoundedImage>
+                        <h5>Sourabh Bujawade</h5>
+                    </div>
+                </div>
+            </div>
+        </center>
     );
 }
 
